ceee4f7bb92d9d5b75376671c28758d59dceb4b4
unknown <vinchevskaja15@gmail.com>
2023-10-12 23:14:16 +0300

    add adaptation 430px

---
+++ src/js/script.js
@@ -0,0 +1,13 @@
+/*let burger_menu_icon = document.querySelector(".burger_menu_icon");
+let subheader_nav = document.querySelector(".subheader_nav");
+burger_menu_icon.addEventListener("click", function () {
+  subheader_nav.classList.toggle("active");
+});
+
+let burger_menu_icon = document.querySelector(".burger_menu_icon");
+let subheader_nav = document.querySelector(".subheader_nav");
+burger_menu_icon.addEventListener("click", function () {
+  burger_menu_icon.classList.toggle("active");
+  subheader_nav.classList.toggle("active");
+});
+*/
